a8c8a5d07adbc3a84296449dfff479aec6576d4d
mostfaswaisi93 <mostfaswaisi93@gmail.com>
2022-06-07 12:53:35 +0300

    Settings Main Sidebar

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -3,10 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Spatie\Translatable\HasTranslations;
 
-class Teacher extends Model
+class Teacher extends Authenticatable
 {
     use HasFactory;
     use HasTranslations;
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
 
     public const HOME = '/dashboard';
     public const STUDENT = '/student/dashboard';
-    public const TEACHER = '/dashboard';
+    public const TEACHER = '/teacher/dashboard';
     public const PARENT = '/dashboard';
 
     /**
@@ -55,6 +55,10 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/student.php'));
+
+            Route::middleware('web')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/teacher.php'));
         });
     }
 
